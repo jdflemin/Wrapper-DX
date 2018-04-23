9688323b07daeed045e1c0b03788d9bb8bd58725
jdflemin <jdf@mchsi.com>
2018-04-23 16:00:09 -0700

    first commit, hello world

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RadialSparkDX
 Node app for interacting with SFDX
+
+will need to use npm link with any changes made in js files
---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log('hello world');
+
+/*
+routing logic based on flags
+call other modules from here
+this acts like a trigger
+*/
+
