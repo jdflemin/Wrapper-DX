a4f64b2d6873714bda00a0fc2eb9171e607df95b
jdflemin <jdf@mchsi.com>
2018-04-25 08:58:45 -0700

    commandModules

--- index.js
+++ index.js
@@ -10,17 +10,15 @@ const open = require('open');
 const argv = require('yargs')
   .version()
   .usage('rsdx <command> [options]')
-  .command(['init [dir]', 'initialize', 'i'], 'Initialize the directory', require('./lib/init'))
+  .command(['init [dir]', 'initialize', 'i'], 'Initialize the directory', require('./commandModules/init'))
   
-  .command('firstName [name]', 'desc of name', require('./lib/printName'))
+  .command('firstName [name]', 'desc of name', require('./commandModules/printName'))
   .command('get', 'make a get HTTP request', {url : {alias: 'u', default: 'http://yargs.js.org/'}})
   .example('rsdx init my-project', 'Initialize `my-project` directory with `default` engine')
   .example('rsdx init my-project --engine turbo', 'Initialize `my-project` directory with `turbo` engine')
-  .command(['docs'], 'Go to the documentation at https://zaiste.net', {}, _ => open('https://zaiste.net'))
   .demandCommand(1, 'You need at least one command before moving on')
   .help('h')
   .alias('h', 'help')
-  .epilogue('for more information, find the documentation at https://zaiste.net')
   .argv;
 
   // console.log('argv._');
