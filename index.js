6e2cec67917869aeadd3c0d65352960232cc7d91
jdflemin <jdfleming91@gmail.com>
2018-04-25 15:45:46 -0700

    reconfig base setup (#6)

--- index.js
+++ index.js
@@ -8,23 +8,33 @@
 const open = require('open');
 
 const argv = require('yargs')
-  .version()
-  .usage('rsdx <command> [options]')
-  .command(['init [dir]', 'initialize', 'i'], 'Initialize the directory', require('./commandModules/init'))
-  .command('firstName [name]', 'desc of name', require('./commandModules/printName'))
-  .command('get', 'make a get HTTP request', {url : {alias: 'u', default: 'http://yargs.js.org/'}})
-  .example('rsdx init my-project', 'Initialize `my-project` directory with `default` engine')
-  .example('rsdx init my-project --engine turbo', 'Initialize `my-project` directory with `turbo` engine')
-  .demandCommand(1, 'You need at least one command before moving on')
-  .help('h')
-  .alias('h', 'help')
-  .argv;
+  // .usage('init <command>')
+  .command('create', 'create command', require('./commandModules/create.js'))
+  .command('delete')
+  .command('list')
+  .argv
 
-  console.log('length: ', argv._.length);
-  console.log('argv._: ', argv._);
-  console.log('argv.$0: ', argv.$0);
-  console.log('argv.verbose: ', argv.verbose);
-  console.log('argv: ', argv);
+
+
+
+// const argv = require('yargs')
+//   .version()
+//   .usage('rsdx <command> [options]')
+//   .command(['init [dir]', 'initialize', 'i'], 'Initialize the directory', require('./commandModules/init'))
+//   .command('firstName [name]', 'desc of name', require('./commandModules/printName'))
+//   .command('get', 'make a get HTTP request', {url : {alias: 'u', default: 'http://yargs.js.org/'}})
+//   .example('rsdx init my-project', 'Initialize `my-project` directory with `default` engine')
+//   .example('rsdx init my-project --engine turbo', 'Initialize `my-project` directory with `turbo` engine')
+//   .demandCommand(1, 'You need at least one command before moving on')
+//   .help('h')
+//   .alias('h', 'help')
+//   .argv;
+
+  // console.log('length: ', argv._.length);
+  // console.log('argv._: ', argv._);
+  // console.log('argv.$0: ', argv.$0);
+  // console.log('argv.verbose: ', argv.verbose);
+  // console.log('argv: ', argv);
 
 /*
 routing logic based on flags
