fb2f294f57b19ca09f007958695eb908143c8a7b
Julian Danciu <jdanciu@radialspark.com>
2018-04-25 17:01:02 -0700

    Julian and Brad stuff (#7)

--- commandModules/create.js
+++ commandModules/create.js
@@ -1,7 +1,19 @@
+var sOrg = require('./create/sOrg.js');
+
 function create(argv) {
-    console.log('****', argv);
-    console.log('length: ', argv._.length);
-    console.log(argv._.pop());
+    console.log(argv._);
+
+    var whatToCreate = argv._[1];
+    var arguments = argv._.slice(2);
+
+    switch(whatToCreate) {
+        case 'sOrg':
+            sOrg.handler(arguments);
+            break;
+        default:
+            console.log("DEFAULT");
+            break;
+    }
 
 }
   
