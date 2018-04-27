d9824539577af0e5e6ed4e951c05e7a4ba8630eb
Bradley Wong <btw47@nau.edu>
2018-04-27 13:33:15 -0700

    Bw 26 april2018 merge (#12)
    * open command working
    
    * install package command added
    
    * added set config command
    
    * added source push command
    
    * added pull command
    
    * added delete sorg command
    
    * added display command

---
+++ commandModules/open/scratchOrg.js
@@ -0,0 +1,43 @@
+/**
+ * @description opens scratch org
+ * @method scratchOrgOpenHandler
+ * @param {array} argv 
+ */
+function scratchOrgOpenHandler(argv) {
+    let baseCommand = 'sfdx force:org:open';
+    const yargs = require('yargs')
+      .option('u', {alias: 'targetusername', demandOption: true, desc: 'STRING: A username or alias for the target org. Overrides the default target org.'})
+      .option('p', {alias: 'path', demandOption: false, desc: 'STRING: Navigation URL path (not including domain).'})
+      .option('r', {alias: 'urlonly', demandOption: false, desc: 'FLAG: Displays a navigation URL, but doesn’t launch your browser.'})
+      .option('j', {alias: 'json', demandOption: false, type:'boolean', desc: 'FLAG: Format output as JSON.'})      
+      .option('loglevel', {alias: 'loglevel', demandOption: false, desc: 'STRING: The logging level for this command invocation. Logs are stored in $HOME/.sfdx/sfdx.log. Permissible values are: trace, debug, info, warn, error, fatal. DEFAULT: error.'})
+
+    const options = yargs.getOptions().alias;
+
+    Object.keys(options).map(opt => {
+        if (yargs.argv[opt] === true) {
+            baseCommand += ` --${options[opt]}`;
+        } else if (yargs.argv[opt] != null) {
+            baseCommand += ` -${opt} ${yargs.argv[opt]}`;
+        }
+    })
+
+    console.log("RUNNING COMMAND: ", baseCommand);
+
+    let exec = require('child_process').exec; 
+
+    exec(baseCommand, (err, stdout, stderr) => {
+      if (err) {
+        // err stuff
+      } else {
+        // not err stuff
+      }
+      // happen regardless stuff
+    });
+    
+}
+
+// export module
+module.exports = {
+    handler: scratchOrgOpenHandler
+};
---
+++ commandModules/pull/source.js
@@ -0,0 +1,43 @@
+/**
+ * @description Pulls changed source from the scratch org to your project to keep them in sync.
+ * @method pullSourceHandler
+ * @param {array} argv 
+ */
+function pullSourceHandler(argv) {
+    let baseCommand = 'sfdx force:source:pull';
+    const yargs = require('yargs')
+        .option('u', {alias: 'targetusername', demandOption: true, desc: 'STRING: A username or alias for the target org. Overrides the default target org.'})
+        .option('f', {alias: 'forceoverwrite', demandOption: false, desc: 'FLAG: Runs the pull command even if conflicts exist. Changes in the scratch org overwrite changes in the project.'})
+        .option('w', {alias: 'wait', demandOption: false, desc: 'MINUTES:  Number of minutes to wait for the command to complete and display results to the terminal window. If the command continues to run after the wait period, the CLI returns control of the terminal window to you. The default is 33 minutes.'})        
+        .option('j', {alias: 'json', demandOption: false, type: 'boolean', desc: 'FLAG: Format output as JSON.'})      
+        .option('loglevel', {alias: 'loglevel', demandOption: false, desc: 'STRING: The logging level for this command invocation. Logs are stored in $HOME/.sfdx/sfdx.log. Permissible values are: trace, debug, info, warn, error, fatal. DEFAULT: error.'})
+
+    const options = yargs.getOptions().alias;
+
+    Object.keys(options).map(opt => {
+        if (yargs.argv[opt] === true) {
+            baseCommand += ` --${options[opt]}`;
+        } else if (yargs.argv[opt] != null) {
+            baseCommand += ` -${opt} ${yargs.argv[opt]}`;
+        }
+    })
+
+    console.log("RUNNING COMMAND: ", baseCommand);
+
+    let exec = require('child_process').exec; 
+
+    exec(baseCommand, (err, stdout, stderr) => {
+      if (err) {
+        // err stuff
+      } else {
+        // not err stuff
+      }
+      // happen regardless stuff
+    });
+    
+}
+
+// export module
+module.exports = {
+    handler: pullSourceHandler
+};
