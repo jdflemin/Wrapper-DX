ff5c178c8d0d4a1f4830018d9476fcf69dd3d4d0
Julian Danciu <jdanciu@radialspark.com>
2018-04-27 15:22:39 -0700

    Update scratchOrg.js

--- commandModules/create/scratchOrg.js
+++ commandModules/create/scratchOrg.js
@@ -4,7 +4,6 @@
  * @param {array} argv 
  */
 function scratchOrgCreateHandler(argv) {
-<<<<<<< HEAD
     let baseCommand = 'sfdx force:org:create';
     const yargs = require('yargs')
       .option('d', {alias: 'duration', demandOption: false, desc: 'default of 7 days, but can range from 1-30'})
@@ -24,30 +23,7 @@ function scratchOrgCreateHandler(argv) {
       }
       // happen regardless stuff
     });
-    
-=======
-  let baseCommand = 'sfdx force:org:create';
-  const yargs = require('yargs')
-    .option('d', {alias: 'duration', demandOption: false, desc: 'default of 7 days, but can range from 1-30'})
-    .option('h', {alias: 'DevHub', demandOption: true, desc: 'alias of parent DevHub to this Scratch Org'})
-    .option('a', {alias: 'alias', demandOption: true, desc: 'alias name of this Scratch Org'})
 
-  baseCommand += ` -f config/project-scratch-def.json -v ${yargs.argv.h} -a ${yargs.argv.a}`;
-
-  if(yargs.argv.d) baseCommand += ` -d ${yargs.argv.d}`;
-
-  let exec = require('child_process').exec; 
-
-  exec(baseCommand, (err, stdout, stderr) => {
-    if (err) {
-      // err stuff
-    } else {
-      // not err stuff
-    }
-    // happen regardless stuff
-  });
-  
->>>>>>> 12dd5d0312b7177fadb4764850f2f7be7a1ee84e
 }
 
 // export module
